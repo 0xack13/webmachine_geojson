88cb102408308afbb76bee27e142fe430b36378b
Saleh Albatati <0xack13@gmail.com>
2014-07-05 14:43:13 +0300

    Modify initial message

--- src/webmachine_geojson_resource.erl
+++ src/webmachine_geojson_resource.erl
@@ -10,4 +10,4 @@
 init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+    {"<html><body>This is gonna be fun!</body></html>", ReqData, State}.
